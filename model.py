9d591c363f99302f2aeea651286ef98645fb56ed
Dariogon1998 <dariopullia@yahoo.it>
2023-10-17 14:51:50 +0200

    Simpler gnn

--- model.py
+++ model.py
@@ -12,8 +12,8 @@ Description:
 import torch
 import torch.nn.functional as F 
 from torch.nn import Linear, BatchNorm1d, ModuleList
-from torch_geometric.nn import TransformerConv, TopKPooling, GCNConv
-from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
+from torch_geometric.nn import TopKPooling, GCNConv
+from torch_geometric.nn import global_mean_pool, global_max_pool
 torch.manual_seed(42)
 
 
@@ -24,75 +24,49 @@ class GNN(torch.nn.Module):
         n_heads = model_params["model_attention_heads"]
         self.n_layers = model_params["model_layers"]
         dropout_rate = model_params["model_dropout_rate"]
-        top_k_ratio = model_params["model_top_k_ratio"]
-        self.top_k_every_n = model_params["model_top_k_every_n"]
         dense_neurons = model_params["model_dense_neurons"]
         first_layer_neurons = model_params["model_first_layer_neurons"]
 
-
-        # self.first_layer = Linear(feature_size, first_layer_neurons)
+        self.first_layer = Linear(feature_size, first_layer_neurons)
         self.conv_layers = ModuleList([])
         self.transf_layers = ModuleList([])
-        self.pooling_layers = ModuleList([])
         self.bn_layers = ModuleList([])
-    
-        # Transformation layer
-        self.conv1 = TransformerConv(feature_size, 
-                                    embedding_size, 
-                                    heads=n_heads, 
-                                    dropout=dropout_rate,
-                                    beta=True) 
 
-        self.transf1 = Linear(embedding_size*n_heads, embedding_size)
+        self.conv1 = GCNConv(first_layer_neurons, embedding_size)
+        self.transf1 = Linear(embedding_size, embedding_size)
         self.bn1 = BatchNorm1d(embedding_size)
 
-        # Other layers
         for i in range(self.n_layers):
-            self.conv_layers.append(TransformerConv(embedding_size, 
-                                                    embedding_size, 
-                                                    heads=n_heads, 
-                                                    dropout=dropout_rate,
-                                                    beta=True))
-
-            self.transf_layers.append(Linear(embedding_size*n_heads, embedding_size))
+            self.conv_layers.append(GCNConv(embedding_size, embedding_size))
+            self.transf_layers.append(Linear(embedding_size, embedding_size))
             self.bn_layers.append(BatchNorm1d(embedding_size))
-            if i % self.top_k_every_n == 0:
-                self.pooling_layers.append(TopKPooling(embedding_size, ratio=top_k_ratio))
-            
 
-        # Linear layers
-        self.linear1 = Linear(embedding_size*2, dense_neurons)
-        self.linear2 = Linear(dense_neurons, int(dense_neurons/2))  
-        self.linear3 = Linear(int(dense_neurons/2), 10)  
+        self.linear1 = Linear(2 * embedding_size, dense_neurons)
+        self.linear2 = Linear(dense_neurons, int(dense_neurons / 2))
+        self.linear3 = Linear(int(dense_neurons / 2), 10)
 
     def forward(self, x, edge_index, batch_index):
-        # Initial transformation
-        # x = torch.relu(self.first_layer(x))
-        x = self.conv1(x, edge_index)
-        x = torch.relu(self.transf1(x))
-        x = self.bn1(x)
 
-        # Holds the intermediate graph representations
-        global_representation = []
+        x = self.first_layer(x)
+        x = F.relu(self.conv1(x, edge_index))
+        x = self.bn1(x)
+        x = self.transf1(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.2, training=self.training)
 
         for i in range(self.n_layers):
-            x = self.conv_layers[i](x, edge_index)
-            x = torch.relu(self.transf_layers[i](x))
+            x = F.relu(self.conv_layers[i](x, edge_index))
             x = self.bn_layers[i](x)
-            # Always aggregate last layer
-            if i % self.top_k_every_n == 0 or i == self.n_layers:
-                x, edge_index, _, batch_index, _, _ = self.pooling_layers[int(i/self.top_k_every_n)](x, edge_index, batch=batch_index)
-                # Add current representation
-                global_representation.append(torch.cat([gmp(x, batch_index), gap(x, batch_index)], dim=1))
-    
-        x = sum(global_representation)
+            x = self.transf_layers[i](x)
+            x = F.relu(x)
+            x = F.dropout(x, p=0.2, training=self.training)
 
-        # Output block
-        x = torch.relu(self.linear1(x))
-        x = F.dropout(x, p=0.8, training=self.training)
-        x = torch.relu(self.linear2(x))
-        x = F.dropout(x, p=0.8, training=self.training)
-        x = self.linear3(x) # in pytorch CrossEntropyLoss() includes softmax
+        # Replace Top-K Pooling with Global Mean Pooling
+        x = global_mean_pool(x, batch_index)
+
+        x = torch.cat([x, x], dim=1)  # Concatenating with itself for simplicity
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
 
         return x
-
